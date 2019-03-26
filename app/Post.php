f856ce46356312f25aa5ae63c4c6d0f96d1fba0e
Yohan Apriyandi <cepoangkuningan@gmail.com>
2019-03-27 06:03:28 +0700

    First commit

---
+++ app/Post.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table= 'posts';
+    protected $fillable = array('title','content','author_id');
+    public $timestamps= true;
+
+    public function Author(){
+        
+    }
+
+}
